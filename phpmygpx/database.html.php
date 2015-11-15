efd4aa147c4fc9d991c63605904dcb86d865769a
Mathieu <mathieu.gravil@gmail.com>
2015-11-15 18:17:40 +0100

    Adding phpmygpx

---
+++ phpmygpx/database.html.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* @version $Id: database.html.php 341 2010-08-22 20:59:49Z eska $
+* @package phpmygpx
+* @copyright Copyright (C) 2008 Sebastian Klemm.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+?>
