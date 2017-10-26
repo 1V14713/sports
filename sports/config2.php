8a29a0779fded0882ddd5b83c9087d45d685eb99
1V14713 <33105663+1V14713@users.noreply.github.com>
2017-10-26 11:26:16 -0700

    sdfsdf
    asdf

--- sports/config2.php
+++ sports/config2.php
@@ -1,8 +1,8 @@
 	<?php
 
 $db_type = 'mysqli';
-$db_host = '10.0.0.66' ;
-$db_name = 'sports';
+$db_host = '10.0.0.5' ;
+$db_name = 'mydb';
 $db_username = 'root';
-$db_password = '3H9hEQSTMrgn';
+$db_password = 'toto';
 ?>
